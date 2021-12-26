f22eaa826e02293fff3d3b51e97f611095b1a6f5
sebastian <sebachuby@gmail.com>
2021-12-26 16:33:47 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -18,7 +18,6 @@ const nuevaMultiplicacion = 10 * null === 40 ;
 // Resuelve el siguiente problema matemático:
 const nuevoModulo = 21 % 5 === null;
 
-
 // En los próximos 22 problemas, deberás completar la función.
 // Todo tu código irá dentro de las llaves de la función.
 // Asegúrate que usas "return" cuando la consola te lo pida.
